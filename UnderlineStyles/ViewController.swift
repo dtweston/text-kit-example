3a8e088299af29e72923eb09c32b5ef5b2bb870a
Dave Weston <dave.weston@binocracy.com>
2015-08-18 22:30:49 -0700

    Basic blank project

--- UnderlineStyles/ViewController.swift
+++ UnderlineStyles/ViewController.swift
@@ -15,6 +15,10 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return .LightContent
+    }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
