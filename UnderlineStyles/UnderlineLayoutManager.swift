aea8c8fe941021fa7545ae238d5d435e17939cb3
Dave Weston <dave.weston@binocracy.com>
2015-08-26 15:31:34 -0500

    Add fancy underline

--- UnderlineStyles/UnderlineLayoutManager.swift
+++ UnderlineStyles/UnderlineLayoutManager.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class UnderlineLayoutManager: NSLayoutManager {
+    
+    func drawFancyUnderlineForRect(rect: CGRect) {
+        let left = CGRectGetMinX(rect)
+        let bottom = CGRectGetMaxY(rect)
+        let width = CGRectGetWidth(rect)
+        
+        let path = UIBezierPath()
+        path.moveToPoint(CGPointMake(left, bottom))
+        var x = left
+        var y = bottom
+        var i = 0
+        while (x <= left + width) {
+            path.addLineToPoint(CGPointMake(x, y))
+            x += 2
+            if i % 2 == 0 {
+                y = bottom + 2.0
+            }
+            else {
+                y = bottom
+            }
+            i++;
+        }
+        
+        UIColor.greenColor().setStroke()
+        path.stroke()
+    }
+    
     override func drawUnderlineForGlyphRange(glyphRange: NSRange, underlineType underlineVal: NSUnderlineStyle, baselineOffset: CGFloat, lineFragmentRect lineRect: CGRect, lineFragmentGlyphRange lineGlyphRange: NSRange, containerOrigin: CGPoint) {
         
         if let container = textContainerForGlyphAtIndex(glyphRange.location, effectiveRange: nil) {
             let boundingRect = boundingRectForGlyphRange(glyphRange, inTextContainer: container)
             let offsetRect = CGRectOffset(boundingRect, containerOrigin.x, containerOrigin.y)
-            CGContextStrokeRect(UIGraphicsGetCurrentContext(), offsetRect)
+            
+            drawFancyUnderlineForRect(offsetRect)
         }
     }
 }
